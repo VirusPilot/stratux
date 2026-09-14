78f8c3b363db1e8d51be6519f57d5fcd36918e0c
VirusPilot <43483458+VirusPilot@users.noreply.github.com>
2026-09-14 11:46:55 +0200

    Revert "Fix: Pass PIGEN_DOCKER_OPTS to build-docker.sh for ARM64 Bookworm base image"
    This reverts commit d3b959ada037863d11755d9356b16fa12755a31f.

--- image_build/build.sh
+++ image_build/build.sh
@@ -30,4 +30,8 @@ fi
 
 # build via docker
 # Force ARM64 Debian Bookworm base image for stratux
-(cd pi-gen && PIGEN_DOCKER_OPTS="--build-arg BASE_IMAGE=arm64v8/debian:bookworm" ./build-docker.sh)
+export PIGEN_DOCKER_OPTS="--build-arg BASE_IMAGE=arm64v8/debian:bookworm"
+#
+# pass PRESERVE_CONTAINER=1 to keep the container in the case of error
+# to enable debugging
+(cd pi-gen && ./build-docker.sh)
